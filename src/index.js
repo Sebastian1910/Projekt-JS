032918e1bc02186b068da2b30b4740c1a618dde3
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-05-09 10:27:09 +0200

    powiązanie plików

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './sass/main.scss';
-import './scripts/fetch'
+import './scripts/fetch';
+import './scripts/footer';
+import './scripts/header_home';
+import './scripts/header_library';
+import './scripts/modal_films';
+import './scripts/modal_queue';
+import './scripts/modal_students';
+import './scripts/modal_watched';
+import './scripts/select_page';
