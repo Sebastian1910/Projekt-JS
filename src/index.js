ac73fc5ae02502689928ac980bd3ba0047c099b7
An9ta <147629509+An9ta@users.noreply.github.com>
2024-05-20 01:04:05 +0200

    13

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import './sass/main.scss';
 import './scripts/fetch';
 import './scripts/footer';
 import './scripts/modal_films';
-import './scripts/modal_card';
 import './scripts/loader';
 import './scripts/search_movies';
--- src/library.js
+++ src/library.js
@@ -1,6 +1,5 @@
 import './sass/main.scss';
 import './scripts/footer';
 import './scripts/modal_films';
-import './scripts/modal_card';
 import './scripts/loader';
 import './scripts/library-films';
